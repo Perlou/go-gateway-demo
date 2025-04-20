20ed17e852f6a3cd1a9be6e6bfcef93b72b9cf21
Perlou <perloukevin@gmail.com>
2025-04-20 17:50:15 +0800

    feat

---
+++ demo/base/close_wait_test/server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "0.0.0.0:9090")
+	if err != nil {
+		fmt.Printf("listen fail, err: %v\n", err)
+		return
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Printf("accept fail, err: %v\n", err)
+			continue
+		}
+		go func(conn net.Conn) {
+			for {
+				var buf [128]byte
+				n, err := conn.Read(buf[:])
+				if err != nil {
+					fmt.Printf("read from connect failed, err: %v\n", err)
+					break
+				}
+				str := string(buf[:n])
+				fmt.Printf("receive from client, data: %v\n", str)
+
+			}
+		}(conn)
+	}
+}
